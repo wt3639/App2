f922fc5c160a38244bbc19dd0c27a1118a783c47
wt3639 <wutao3639@163.com>
2017-03-03 11:01:25 +0800

    change the app name
    change the app name to StressTest

--- App2/Properties/AssemblyInfo.cs
+++ App2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("App2")]
+[assembly: AssemblyTitle("StressTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("App2")]
+[assembly: AssemblyProduct("StressTest")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
